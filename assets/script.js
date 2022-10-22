6d1b7c1663c07d6dfb367db81bb539f94d0c0974
Brandon <brandonkly@ucla.edu>
2022-10-22 02:25:47 -0700

    html start and javascript pseudocode

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// set variables
+// set functions for query selection and create element
+// create homepage
+// use function to switch to next question
+// use key value pairs to store answers and true/false-ness
+// use setInterval to time
+// use if time=0 to end game
+// use gamerunning variable with boolean value
+// use local storage to set leaderboard
